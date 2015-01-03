4135daf68558f68bd60821034b17be8e7e0de771
Kevin Green <thedudem4n@gmail.com>
2015-01-03 16:42:04 -0500

    original tweaking, removing extra routes setting up typography

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ Router.configure({
   notFoundTemplate: 'notFound',
   fastRender: true,
   waitOn: function() {
-    return [orion.subs.subscribe('dictionary'), orion.subs.subscribe('entity', 'works'), orion.subs.subscribe('entity', 'posts'), orion.subs.subscribe('entity', 'pages')];
+    return [orion.subs.subscribe('dictionary'), orion.subs.subscribe('entity', 'works')];
   }
 });
 
@@ -79,66 +79,6 @@ Router.map(function() {
       }
       SEO.set({
         title: contact + ' | ' + dict.seoTitle,
-      });
-    }
-  });
-
-  // Blog and individual blog posts
-
-  this.route('blog', {
-    path: '/blog',
-    fastRender: true,
-    onAfterAction: function() {
-      dict = orion.dictionary.collection.findOne();
-      SEO.set({
-        title: 'Blog | ' + dict.seoTitle,
-      });
-    }
-  });
-  this.route('post', {
-    path: '/blog/:slug',
-    layoutTemplate: 'layout',
-    fastRender: true,
-    waitOn: function() {
-      return [orion.subs.subscribe('entity', 'posts', {slug: this.params.slug})];
-    },
-    data: function() {
-      return orion.entities.posts.collection.findOne({slug: this.params.slug});
-    },
-    onAfterAction: function() {
-      post = orion.entities.posts.collection.findOne({slug: this.params.slug});
-      dict = orion.dictionary.collection.findOne();
-      title = post.title + ' | ' + dict.seoTitle;
-      SEO.set({
-        title: title,
-      });
-    }
-  });
-
-  // General Pages
-  this.route('page', {
-    path: ':slug',
-    layoutTemplate: 'layout',
-    fastRender: true,
-    waitOn: function() {
-      return [orion.subs.subscribe('entity', 'pages', {slug: this.params.slug})];
-    },
-    data: function() {
-      page = orion.entities.pages.collection.findOne({slug: this.params.slug});
-      var response = this.response;
-      console.log(response);
-      if (page === undefined) {
-        // this.redirect('base');
-        return null;
-      }
-      return orion.entities.pages.collection.findOne({slug: this.params.slug});
-    },
-    onAfterAction: function() {
-      post = orion.entities.pages.collection.findOne({slug: this.params.slug});
-      dict = orion.dictionary.collection.findOne();
-      title = page.title + ' | ' + dict.seoTitle;
-      SEO.set({
-        title: title,
       });
     }
   });
