ee436f954315170b0419f2a00d7610bec226b55a
Kevin Green <kevin@swedenunlimited.com>
2015-01-05 13:59:26 -0500

    actually use bower correctly

--- schema/work.js
+++ schema/work.js
@@ -1,42 +1,42 @@
 orion.addEntity('works', {
-	image: orion.attribute('file', {
-		label: "Featured Image or specify a color fallback",
-		optional: true
-	}),
-	name: {
-		type: String,
-		label: "Project Name"
-	},
-	year: {
-		type: String,
-		label: "Year",
-		optional: true,
-	},
-	role: {
-		type: String,
-		label: "Role",
-		optional: true
-	},
+  image: orion.attribute('file', {
+    label: "Featured Image or specify a color fallback",
+    optional: true
+  }),
+  name: {
+    type: String,
+    label: "Project Name"
+  },
+  year: {
+    type: String,
+    label: "Year",
+    optional: true,
+  },
+  role: {
+    type: String,
+    label: "Role",
+    optional: true
+  },
   client: {
     type: String,
     label: "Client Name",
   },
-	slug: {
-		type: String,
-		label: "Slug",
-		optional: true
-	},
-	description: {
-		type: String,
-		label: "Short Description"
-	},
-	// detailImages: {
-	// 	type: [orion.attributes.image],
-	// 	label: "Detail Images"
-	// },
-	projectType: {
+  slug: {
     type: String,
-		label: "Multiple projects or one?",
+    label: "Slug",
+    optional: true
+  },
+  description: {
+    type: String,
+    label: "Short Description"
+  },
+  // detailImages: {
+  // 	type: [orion.attributes.image],
+  // 	label: "Detail Images"
+  // },
+  projectType: {
+    type: String,
+    label: "Multiple projects or one?",
     optional: true,
     autoform: {
       afFieldInput: {
@@ -44,25 +44,26 @@ orion.addEntity('works', {
       }
     }
   },
-	projects: {
-      type: Array,
-      optional: true,
-      minCount: 0,
-   },
-   "projects.$": {
-      type: Object
-   },
-   "projects.$.name": {
-      type: String
-   },
-   "projects.$.image": orion.attribute('file', {
-			label: 'Image',
-	}),
+  projects: {
+    type: Array,
+    optional: true,
+    minCount: 0,
+  },
+  "projects.$": {
+    type: Object
+  },
+  "projects.$.name": {
+    type: String
+  },
+  "projects.$.image": orion.attribute('file', {
+    label: 'Image',
+  }),
 }, {
-	sidebarName: 'Works',
-	pluralName: 'Works',
-	singularName: 'Work',
-	defaultIndexTableFields: [
-		{key:'projectName', label: 'Project'},
-	]
+  sidebarName: 'Works',
+  pluralName: 'Works',
+  singularName: 'Work',
+  tableColumns: [{
+    data: 'name',
+    title: 'Name'
+  }, ]
 });
