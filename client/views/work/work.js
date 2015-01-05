11ca4e05b599a68199382cfda3af0cc7e033da3f
Kevin Green <kevin@swedenunlimited.com>
2015-01-05 15:51:56 -0500

    cleaned up some initial templating

--- client/views/work/work.js
+++ client/views/work/work.js
@@ -5,23 +5,11 @@ Template.work.helpers({
 });
 
 Template.workBlock.helpers({
-  tools: function (tools) {
-    var classes = '';
-    for (var i = 0; i < tools.length; i++) {
-      classes += tools[i] + ' ';
-    }
-    return classes;
-  }
+
 });
 
 Template.workSingle.helpers({
-    icons: function (tools) {
-      var icon = '';
-      for (var i = 0; i < tools.length; i++) {
-        icon += '<i class="' + tools[i] + '"></i>';
-      }
-      return icon;
-    }
+
 });
 
 Template.workSingle.rendered = function() {
