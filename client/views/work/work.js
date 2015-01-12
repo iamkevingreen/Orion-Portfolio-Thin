6cf25615fc96e19e9d6fbf298e440ab03abdc0ac
Kevin Green <kevin@swedenunlimited.com>
2015-01-12 18:03:32 -0500

    thinking about initial animations

--- client/views/work/work.js
+++ client/views/work/work.js
@@ -28,4 +28,9 @@ Template.workSingle.rendered = function() {
   $('header').addClass('small');
   $('.content img').wrap( "<div class='content-wrap'></div>");
   $('.content-wrap').prepend("<div class='controls'></div>");
+
+  _.each($('.animated'), function() {
+    $(this).addClass('show');
+    console.log(this)
+  });
 };
